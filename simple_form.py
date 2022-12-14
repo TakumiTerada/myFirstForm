8ebfdf0c6c788aacb4c6ef2bff7e862e26d5a8a4
takum <takumi0825temple@outlook.jp>
2022-12-14 15:18:33 +0900

    initial commit

--- simple_form.py
+++ simple_form.py
@@ -8,8 +8,12 @@ def index():
 
 @app.route('/procForm', methods=['POST'])
 def processPost():
-    msg = request.form['msg']
-    return render_template("result.html", message = msg)
+    msg1 = request.form['msg1']
+    msg2 = request.form['msg2']
+    num1 = int(msg1)
+    num2 = int(msg2)
+    msg=num1 + num2
+    return render_template("result.html", message1 = msg1 , message2 = msg2 ,message = msg)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug = True)
